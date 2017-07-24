837efb00b62fe8fe2850055aedfb16fbca808dba
John Whitson <john.whitson@gmail.com>
2017-07-24 16:05:00 -0600

    Initial commit

---
+++ lib/ubiquity/vdms.rb
@@ -0,0 +1,7 @@
+require "ubiquity/vdms/version"
+
+module Ubiquity
+  module Vdms
+    # Your code goes here...
+  end
+end
---
+++ lib/ubiquity/vdms/version.rb
@@ -0,0 +1,5 @@
+module Ubiquity
+  module Vdms
+    VERSION = "0.1.0"
+  end
+end
